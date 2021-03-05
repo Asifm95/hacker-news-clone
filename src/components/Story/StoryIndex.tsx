9ed7b0dc7ff37c818a990d296499c618b0da76f3
Asif Mohammed <asifm2882@gmail.com>
2021-03-05 15:22:12 +0400

    add StoryIndex component

---
+++ src/components/Story/StoryIndex.tsx
@@ -0,0 +1,19 @@
+import { HStack, Heading, Icon } from '@chakra-ui/react';
+import React from 'react';
+import { FiTriangle } from 'react-icons/fi';
+import { zeroPad } from '../../utils/helper';
+
+type StoryIndexProps = {
+    index: number;
+    width: string;
+};
+
+const StoryIndex: React.FC<StoryIndexProps> = ({ index, ...rest }) => {
+    return (
+        <HStack h="100%" justify="space-between" {...rest}>
+            <Heading fontSize="lg">{zeroPad(index, 2)}</Heading>
+            <Icon as={FiTriangle} fill="#f56565" color="#f56565" />
+        </HStack>
+    );
+};
+export default StoryIndex;
