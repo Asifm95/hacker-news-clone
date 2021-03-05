83ee4a9f5cc38e9a45f3d63eafe16e6abb4cb38b
Asif Mohammed <asifm2882@gmail.com>
2021-03-05 19:55:21 +0400

    extend default theme

---
+++ src/theme.ts
@@ -0,0 +1,7 @@
+import { extendTheme } from '@chakra-ui/react';
+// 2. Call `extendTheme` and pass your custom values
+export const theme = extendTheme({
+    colors: {
+        primary: '#f56565',
+    },
+});
