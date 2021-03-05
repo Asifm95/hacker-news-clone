90f08e7621d5e7ee5cb7ccee3438459d477db32e
Asif Mohammed <asifm2882@gmail.com>
2021-03-05 16:23:16 +0400

    add NewStories to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import * as React from 'react';
 import { ChakraProvider, Box, theme } from '@chakra-ui/react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-// import { ColorModeSwitcher } from './ColorModeSwitcher';
 import TopStories from './containers/TopStories';
 import Navbar from './components/Navbar/Navbar';
+import NewStories from './containers/NewStories';
 export const App = () => (
     <ChakraProvider theme={theme}>
         <Box minH="100vh">
-            <Navbar />
             <BrowserRouter>
+                <Navbar />
                 <Switch>
                     <Route exact path="/" component={TopStories} />
-                    <Route exact path="/new" component={TopStories} />
+                    <Route exact path="/new" component={NewStories} />
                 </Switch>
             </BrowserRouter>
-            {/* <ColorModeSwitcher justifySelf="flex-end" /> */}
         </Box>
     </ChakraProvider>
 );
