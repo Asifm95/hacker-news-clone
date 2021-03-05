46a3ff260a53c166bf8258630da7dab1b086ff9a
Asif Mohammed <asifm2882@gmail.com>
2021-03-05 15:18:11 +0400

    setup api endpoints

---
+++ src/apis/index.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { IStory, IStoryIds } from '../types/StoryInterface';
+
+export const BASE_URL = 'https://hacker-news.firebaseio.com/v0/';
+export const newStoriesUrl = `newstories.json`;
+export const topStoriesUrl = `topstories.json`;
+export const jobStoriesUrl = `jobstories.json`;
+export const storyUrl = `item/`;
+
+const api = axios.create({
+    baseURL: BASE_URL,
+});
+
+export const getStory = async (storyId: number): Promise<IStory> => {
+    const result = await api.get<IStory>(`${storyUrl + storyId}.json`);
+    return result.data;
+};
+
+export const getNewStoryIds = async (): Promise<IStoryIds> => {
+    const result = await api.get<IStoryIds>(newStoriesUrl);
+    return result.data;
+};
+export const getTopStoryIds = async (): Promise<IStoryIds> => {
+    const result = await api.get<IStoryIds>(topStoriesUrl);
+    return result.data;
+};
